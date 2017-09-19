197785e9f0e1eaac443a76492d46ad56c480fe47
Oleg Bukatchuk <oleg@bukatchuk.com>
2017-09-19 20:49:44 +0300

    up

---
+++ winlotto.c
@@ -0,0 +1,37 @@
+//
+// Created by Oleg Bukatchuk on 19/09/2017.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <windows.h>
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    int i, r, temp, nums[50];
+    char buf[4], str[50] = {"Your Six Lucky Numbers Are: "};
+
+    srand(time(NULL));
+
+    for (i = 0; i < 50; i++) {
+        nums[i] = i;
+    }
+
+    for (i = 1; i < 50; i++) {
+        r = (rand() % 49) + 1;
+        temp = nums[i];
+        nums[i] = nums[r];
+        nums[r] = temp;
+    }
+
+    for (i = 1; i < 7; i++) {
+        sprintf(buf, "%d", nums[i]);
+        strcat(buf, " ");
+        strcat(str, buf);
+    }
+
+    MessageBox(NULL, str, "Lotto Number Picker", MB_OK | MB_ICONEXCLAMATION);
+
+    return 0;
+}
